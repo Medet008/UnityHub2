2bfbf308b0fdfbdb3a5c7ca7e1132e2727dcae0f
medet <malyka.maryam@gmail.com>
2022-02-14 22:32:41 +0600

    до делоано

---
+++ Zadacha5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Zadacha5
+{
+    internal class Program
+    {
+
+        static double GetMass()
+        {
+           double mass = double.Parse(Console.ReadLine());  
+            return mass;
+        }
+        static double GetRost()
+        {
+            double rost = double.Parse(Console.ReadLine());
+            return rost;
+        }
+        static void Main(string[] args)
+            {
+              Console.WriteLine("введите ваш рост");
+            double rost = GetRost() / 100; 
+              Console.WriteLine("введите ваш вес");
+              double mass = GetMass();
+              double rostMass = mass / (rost * rost);
+
+            double massIndexMax = 24.99;
+            double massIndexMin = 18.5;
+
+            double indexMax;
+            double indexMin; 
+            double result;
+             
+               if(rostMass > massIndexMax)
+            {
+                indexMax = massIndexMax * (rost * rost);
+                result = Math.Abs(mass - indexMax);
+                Console.WriteLine($"ваш индекс масса тела состовляет {rostMass:F2}");
+                Console.WriteLine($"вам необхадимо скинуть вес {result:F2}");
+            } else if (rostMass < massIndexMin)
+            {
+                indexMin= massIndexMin * (rost * rost);
+                result = Math.Abs(mass - indexMin);
+                Console.WriteLine($"ваш индекс масса тела состовляет {rostMass:F2}");
+                Console.WriteLine($"вам необхадимо набрать вес {rostMass:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"ваш индекс масса тела состовляет {rostMass:F2}");
+                Console.WriteLine("ваш вес норм");
+            }
+            
+        }
+    }
+}
---
+++ Zadacha6/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+namespace Zadacha6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число диапазоне от одонго до определенных чисел ");
+            int amountNumber = 0;
+            int num = GetNum();
+            DateTime dateTimeStart = DateTime.Now;
+
+            for (int i = 1; i<num; i++)
+            {
+                if(GetChekNumber(i))
+                {
+                    amountNumber++;
+                }
+            }
+
+            Console.WriteLine("vremia raboti program");
+            Console.WriteLine(DateTime.Now - dateTimeStart);
+            Console.WriteLine($"заданном дипазоне содержатся хороших чисел: {amountNumber}");
+            Console.ReadKey();
+        }
+
+        static int GetNum()
+        {
+            while (true)
+            {
+                bool number = int.TryParse(Console.ReadLine(), out int num);
+                if (!number)
+                {
+                    Console.WriteLine("ввeден неверно требуется ввести число занова");
+                }
+                else
+                {
+                    return num;
+                }
+            }
+        }
+
+        static bool GetChekNumber(int number)
+        {
+            string s = Convert.ToString(number);
+            int sum = 0;
+            for(int i = 0; i < s.Length; i++)
+            {
+                sum += s[i] - '0';
+               
+            }
+            if (number % sum == 0)
+
+                return true;
+
+            else
+                return false;
+        }
+    }
+}
